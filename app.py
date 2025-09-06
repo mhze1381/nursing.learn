19f173c01b4dad6d7b6732eefdc0685f38f0172c
Mohammad hze <mohammad.mohsan95@gmail.com>
2025-09-06 21:49:19 +0330

    learning flask

--- app.py
+++ app.py
@@ -4,15 +4,14 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
-files = [
-    {"name": "فایل PDF نمونه", "url": "https://drive.google.com/uc?export=download&id=FILE_ID1"},
-    {"name": "ویدیو نمونه", "url": "https://drive.google.com/uc?export=download&id=FILE_ID2"},
-    {"name": "عکس نمونه", "url": "https://drive.google.com/uc?export=download&id=FILE_ID3"}
-]
-
 @app.route("/")
 def index():
-    return render_template("index.html", files=files)
+    return render_template("index.html")
+
+@app.route("/aski")
+def OnAski():
+    return render_template("aski.html")
+
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
