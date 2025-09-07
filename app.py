f83d3f8645d6d3c6d306d79356c730591356ba30
Mohammad hze <mohammad.mohsan95@gmail.com>
2025-09-07 16:32:22 +0330

    biggerthan

--- app.py
+++ app.py
@@ -13,8 +13,15 @@ def aski():
     return render_template("aski.html")
 
 @app.route("/emergency")
-def Emergency():
+def emergency():
     return render_template("Emergency.html")
+@app.route("/nezam")
+def nezam():
+    return render_template("nezam.html")
+@app.route("/learn")
+def learn():
+    return render_template("learn.html")
+
 
 
 if __name__ == "__main__":
