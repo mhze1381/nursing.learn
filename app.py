90270fd200c05ee4045edc1fd8901add731b3077
Mohammad hze <mohammad.mohsan95@gmail.com>
2025-09-28 13:19:15 +0330

    fix route

--- app.py
+++ app.py
@@ -8,26 +8,15 @@ app = Flask(__name__)
 def index():
     return render_template("index.html")
 
-@app.route("/aski")
-def aski():
-    return render_template("aski.html")
 
-@app.route("/emergency")
-def Emergency():
-    return render_template("Emergency.html")
-@app.route("/nezam")
-def nezam():
-    return render_template("nezam.html")
-@app.route("/learn")
-def learn():
-    return render_template("learn.html")
-@app.route("/tarh")
-def tarh():
-    return render_template("tarh.html")
-@app.route("/vam")
-def vam():
-    return render_template("vam.html")
-
+@app.route("/<page>")
+def show_page(page):
+    page = page.lower()  # همه حروف کوچیک بشه
+    file_path = os.path.join(app.template_folder, f"{page}.html")
+    if os.path.exists(file_path):
+        return render_template(f"{page}.html")
+    else:
+        return "صفحه مورد نظر یافت نشد."
 
 if __name__ == "__main__":
     port = int(os.environ.get("PORT", 5000))
